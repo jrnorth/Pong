49a7d48358cfde1946a8ce9fea6f94b19535577c
Joseph North <joernorth@gmail.com>
2014-05-15 12:48:01 -0500

    Added initial files

---
+++ core/src/north/joseph/pong/Pong.java
@@ -0,0 +1,26 @@
+package north.joseph.pong;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Pong extends ApplicationAdapter {
+	@Override
+	public void create () {
+
+	}
+	
+	@Override
+	public void dispose() {
+
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+}
