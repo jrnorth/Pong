ba270c440ec5d82d2091e9951187ccb9e601d279
Joseph North <joernorth@gmail.com>
2014-05-23 11:37:04 -0500

    Removed shadow fonts and added files for the UI assets.

--- core/src/north/joseph/pong/helpers/AssetManager.java
+++ core/src/north/joseph/pong/helpers/AssetManager.java
@@ -2,28 +2,23 @@ package north.joseph.pong.helpers;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class AssetManager {
 	public static BitmapFont font;
-	public static BitmapFont shadow;
 	public static BitmapFont scoreFont;
-	public static BitmapFont scoreShadow;
+	public static Skin uiSkin;
 	
 	public static void load() {
 		font = new BitmapFont(Gdx.files.internal("text.fnt"));
         font.setScale(.5f, .5f);
-        shadow = new BitmapFont(Gdx.files.internal("shadow.fnt"));
-        shadow.setScale(.5f, .5f);
         scoreFont = new BitmapFont(Gdx.files.internal("text.fnt"));
         scoreFont.setScale(.36f, .36f);
-        scoreShadow = new BitmapFont(Gdx.files.internal("shadow.fnt"));
-        scoreShadow.setScale(.36f, .36f);
+        uiSkin = new Skin(Gdx.files.internal("uiskin.json"));
 	}
 	
 	public static void dispose() {
 		font.dispose();
-		shadow.dispose();
 		scoreFont.dispose();
-		scoreShadow.dispose();
 	}
 }
